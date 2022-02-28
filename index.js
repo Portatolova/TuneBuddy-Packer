38d52f503464daf7688aee87d7e9e1f9f69832e1
Carl Voller <carlvoller8@gmail.com>
2022-02-28 18:11:20 +0800

    Updated with logging

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ app.get("/pull", (req, res) => {
     const cmd = `
         cd /home/carlvoller/TuneBuddy-Web;
         git pull;
-        npm run setup;
-        npm run build;
+        ./build.sh;
         docker-compose up -d;
     `
-    exec(cmd, () => {
+    exec(cmd, (err, stdout, stderr) => {
+        console.log(err);
+        console.log(stdout);
+        console.log(stderr);
         return res.status(200).end();
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log("Listening on PORT 3000"));
