7442b699929f3cea96e0d8f6cecbc4e575e57cac
Carl Voller <carlvoller8@gmail.com>
2022-02-28 18:55:28 +0800

    removed packer from dockerfile cause that was a stupid idea, restored original commands

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -t tunebuddy-packer .
+npm i
--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const spawn = require('child_process').spawn;
 const app = express();
 
 app.get("/pull", (req, res) => {
-    const cmd = `git pull`
+    const cmd = `git pull && ./build.sh && docker-compose up -d && docker rmi $(docker images --filter "dangling=true" -q --no-trunc)`
     let proc = spawn(cmd, { cwd: "/home/carlvoller/TuneBuddy-Web", shell: "/bin/bash" });
 
     proc.stdout.on('data', (data) => console.log(data.toString()));
-    proc.stderr.on('data', (data) => console.log(data.toString()));
+    proc.stderr.on('data', (data) => console.error(data.toString()));
     proc.on('exit', () => res.status(200).end());
 });
 
